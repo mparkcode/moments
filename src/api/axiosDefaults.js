09ac329b591ce79683e1fc33e98397479b141fa1
michael <mparkcode@gmail.com>
2022-05-25 11:46:38 +0900

    interceptors

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -2,4 +2,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = 'https://mpark-moments-drf-api.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.withCredentials = true
+axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
