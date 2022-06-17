c5030094e86a412f5260835ddbc574de95f2c4c4
michael <mparkcode@gmail.com>
2022-06-17 15:13:07 +0900

    editing and commenting

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = 'https://mpark-moments-drf-api.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.headers.put['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
 
 export const axiosReq = axios.create();
--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -13,6 +13,8 @@ import Post from "./Post";
 
 import NoResults from '../../assets/no-results.png'
 import Asset from "../../components/Asset";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostsPage({message, filter=""}) {
     const [posts, setPosts] = useState({results: []})
@@ -33,7 +35,12 @@ function PostsPage({message, filter=""}) {
         }
 
         setHasLoaded(false)
-        fetchPosts()
+        const timer = setTimeout(() => {
+            fetchPosts()
+        }, 1000)
+        return () => {
+            clearTimeout(timer)
+        }
     }, [filter, query, pathname])
   
   return (
@@ -56,9 +63,18 @@ function PostsPage({message, filter=""}) {
         {hasLoaded ? (
             <>
                 {posts.results.length ? (
-                    posts.results.map((post) => (
-                        <Post key={post.id} {...post} setPosts={setPosts} />
-                    ))
+                    <InfiniteScroll 
+                        children={
+                            posts.results.map((post) => (
+                                <Post key={post.id} {...post} setPosts={setPosts} />
+                            ))
+                        }
+                        dataLength={posts.results.length}
+                        loader={<Asset spinner />}
+                        hasMore={!!posts.next}
+                        next={() => fetchMoreData(posts, setPosts)}
+                    />
+                    
                 ) : (
                     <Container className={appStyles.Content}>
                         <Asset src={NoResults} message={message}/>
