68d4e7492c32f4b53cfd1d3d7095bbd5fbac7e6a
michael <mparkcode@gmail.com>
2022-06-18 13:22:43 +0900

    comments and stuff

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -10,6 +10,10 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Comment from "../comments/Comment";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostPage() {
   const { id } = useParams()
@@ -22,11 +26,12 @@ function PostPage() {
   useEffect(() => {
       const handleMount = async () => {
           try{
-            const [{data: post}] = await Promise.all([
-                axiosReq.get(`/posts/${id}`)
+            const [{data: post}, {data: comments}] = await Promise.all([
+                axiosReq.get(`/posts/${id}`),
+                axiosReq.get(`/comments/?post=${id}`)
             ])
             setPost({results: [post]})
-            console.log(post)
+            setComments(comments)
           } catch(err) {
               console.log(err)
           }
@@ -49,9 +54,25 @@ function PostPage() {
             setPost={setPost}
             setComments={setComments}
           />
-          ) : comments.results.length ? (
-            "Comments"
-          ) : null}
+          ) : ('log in to comment')}
+          {comments.results.length ? (
+            <InfiniteScroll 
+            children={
+              comments.results.map(comment => (
+                <Comment key={comment.id} {...comment} setComments={setComments} setPost={setPost}/>
+              ))
+            }
+            dataLength={comments.results.length}
+            loader={<Asset spinner />}
+            hasMore={!!comments.next}
+            next={() => fetchMoreData(comments, setComments)}
+        />
+            
+          ) : currentUser? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments...yet</span>
+          )}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
